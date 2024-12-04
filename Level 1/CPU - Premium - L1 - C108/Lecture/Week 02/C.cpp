e693f7836f2d9625695bbba5680d442c4f47d29c
abdualla1 <38115506+abdualla1@users.noreply.github.com>
2024-12-04 16:01:48 +0200

    Lecture Week2

---
+++ Level 1/CPU - Premium - L1 - C108/Lecture/Week 02/C.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int sum = a + b, sub = a - b, prod = a * b;
+    if (sum >= sub && sum >= prod)
+        cout << sum << '\n';
+    else if (sub >= sum and sub >= prod)
+        cout << sub << '\n';
+    else
+        cout << prod << '\n';
+    return 0;
+}
