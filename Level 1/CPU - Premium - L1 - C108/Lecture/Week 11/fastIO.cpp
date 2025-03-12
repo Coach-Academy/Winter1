9a8bad80258116b1e6607f5d346afad642e22740
Folka95 <mohamedkgm95@gmail.com>
2025-03-12 11:15:15 +0200

    Lecture Session Week 11

---
+++ Level 1/CPU - Premium - L1 - C108/Lecture/Week 11/fastIO.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // this fast up the input and output so you might
+    // avoid TLE in problems which have lage input or output
+    std::ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    return 0;
+}
